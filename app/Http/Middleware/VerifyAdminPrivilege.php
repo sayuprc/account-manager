11f4d91c7851073a8d48a1b8b9b38c6596ec5dae
sayuprc <mkpoto15@gmail.com>
2023-05-28 22:38:43 +0900

    Created middlewares for user information verification

---
+++ app/Http/Middleware/VerifyAdminPrivilege.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class VerifyAdminPrivilege
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (! $request->user()->is_admin) {
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,21 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', [RegisterController::class, 'showRegistrationForm']);
-Route::post('/register', [RegisterController::class, 'store']);
+Route::middleware(['guest'])->group(function () {
+    Route::get('/register', [RegisterController::class, 'showRegistrationForm']);
+    Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/login', [LoginController::class, 'showLoginForm']);
-Route::post('/login', [LoginController::class, 'handle']);
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'handle']);
+});
 
-// グルーピングする必要あり
-Route::get('/admin/users', [IndexController::class, 'index']);
+Route::middleware(['auth', 'activity'])->group(function () {
+    Route::middleware(['admin'])->group(function () {
+        Route::get('/admin/users', [IndexController::class, 'index']);
 
-Route::get('/admin/users/{id}/edit', [EditController::class, 'show'])->whereUuid('id');
-Route::post('/admin/users/{id}/edit', [EditController::class, 'store'])->whereUuid('id');
+        Route::get('/admin/users/{id}/edit', [EditController::class, 'show'])->whereUuid('id');
+        Route::post('/admin/users/{id}/edit', [EditController::class, 'store'])->whereUuid('id');
 
-Route::post('/admin/users/{id}/delete', [DeleteController::class, 'delete'])->whereUuid('id');
+        Route::post('/admin/users/{id}/delete', [DeleteController::class, 'delete'])->whereUuid('id');
+    });
+});
